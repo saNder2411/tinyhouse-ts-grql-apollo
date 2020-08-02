e1383d983a3b61bdcc3024dc7e01a9fa5cb953f5
Alexander Olshanskiy <sander2411ukr@gmail.com>
2020-08-02 09:50:36 +0300

    add schema

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,34 +1,12 @@
 import express from 'express';
-import bodyParser from 'body-parser';
-import { listings } from './listings';
-
-interface Req {
-  body: {
-    id: string;
-  };
-}
+import { ApolloServer } from 'apollo-server-express';
+import { schema } from './graphql';
 
 const app = express();
 const port = 9000;
 
-app.use(bodyParser.json());
-
-// app.get('/', (_req, res) => res.send('Hello!'));
-
-app.get('/listings', (_req, res) => {
-  return res.send(listings);
-});
-
-app.post('/delete-listing', (req: Req, res) => {
-  const id = req.body.id;
-  for (let i = 0; i < listings.length; i++) {
-    if (listings[i].id === id) {
-      return res.send(listings.splice(i, 1));
-    }
-  }
-
-  return res.send('failed to delete listing');
-});
+const server = new ApolloServer({ schema });
+server.applyMiddleware({ app, path: '/api' });
 
 app.listen(port);
 
